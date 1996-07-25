7542b5497726737fe8f449d9e39a39dd16309495
scrappy <scrappy>
1996-07-25 20:45:05 +0000

    Add in port to DG/UX
    Submitted by: "Brian E. Gallew" <geek+@cmu.edu>

---
+++ src/backend/port/dgux/port.c
@@ -0,0 +1,13 @@
+/*-------------------------------------------------------------------------
+ *
+ * port.c--
+ *    Linux-specific routines
+ *
+ * Copyright (c) 1994, Regents of the University of California
+ *
+ *
+ * IDENTIFICATION
+ *    $Header: /home/rubik/work/pgcvs/CVSROOT/pgsql/src/backend/port/dgux/Attic/port.c,v 1.1 1996-07-25 20:44:00 scrappy Exp $
+ *
+ *-------------------------------------------------------------------------
+ */
