6e2db4ac196e7ad03c1826da3935c65468fa1567
scrappy <scrappy>
1996-08-14 04:54:45 +0000

    Here's a small makefile patch that corrects the following bug:  The makefiles don't indicate that the libpq.a library is a dependency of all the /bin programs.  So if the library changes, the /bin programs don't get remade.
    Submitted by: Bryan Henderson <bryanh@giraffe.netgate.net>

--- src/mk/postgres.prog.mk
+++ src/mk/postgres.prog.mk
@@ -10,13 +10,13 @@
 #
 #
 # IDENTIFICATION
-#    $Header: /home/rubik/work/pgcvs/CVSROOT/pgsql/src/mk/Attic/postgres.prog.mk,v 1.2 1996-08-13 07:48:33 scrappy Exp $
+#    $Header: /home/rubik/work/pgcvs/CVSROOT/pgsql/src/mk/Attic/postgres.prog.mk,v 1.3 1996-08-14 04:54:45 scrappy Exp $
 #
 #-------------------------------------------------------------------------
 
 PROGOBJS:= $(SRCS:%.c=%.o)
 
-$(PROG):  $(addprefix $(objdir)/,$(PROGOBJS))
+$(PROG):  $(addprefix $(objdir)/,$(PROGOBJS)) $(LIB_DEP)
 	$(CC) $(LDFLAGS) -o $(objdir)/$(@F) $(addprefix $(objdir)/,$(PROGOBJS)) $(LD_ADD)
 
 CLEANFILES+= $(PROGOBJS) $(PROG)
