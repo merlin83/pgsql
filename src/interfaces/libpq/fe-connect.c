6732837a54cd6f935fb0e802daea29c780769df2
scrappy <scrappy>
1997-03-25 09:08:06 +0000

    Here's two more diffs...
    The first fixes a warning from gcc about the assignment within the condition.
    The extra set of parens should not make a difference, but with -Werror, they
    are necessary.
    
    The second fixes an "ln -s" invocation that assumes the current directory is
    implicitly the target if not specified.  Not true in all cases, and again, it
    should not make a difference except to those implementation that it does.
    
    From: "Michael P. Snyder" <msnyder@hawkeye.huntersmoon.com>

--- src/interfaces/libpq/fe-connect.c
+++ src/interfaces/libpq/fe-connect.c
@@ -7,7 +7,7 @@
  *
  *
  * IDENTIFICATION
- *    $Header: /home/rubik/work/pgcvs/CVSROOT/pgsql/src/interfaces/libpq/fe-connect.c,v 1.26 1997-03-25 02:37:21 scrappy Exp $
+ *    $Header: /home/rubik/work/pgcvs/CVSROOT/pgsql/src/interfaces/libpq/fe-connect.c,v 1.27 1997-03-25 09:08:06 scrappy Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -536,7 +536,7 @@ connectDB(PGconn *conn)
 			{
 			const char *val;
 			
-			if(val = getenv(eo->envName))
+			if((val = getenv(eo->envName)))
 				{
 				PGresult *res;
 				
