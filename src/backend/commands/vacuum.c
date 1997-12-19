5d9a100b2a9611858ace47039ecdd443d88ef064
scrappy <scrappy>
1997-12-19 02:09:10 +0000

    More cleanups.  I can now compile without PORTNAME being defined n Makefile.global.
    End result, if all goes well, should allow for much easier porting, since
    there will no longer be a concept of a "port".  Most, if not everything,
    *should* be determined by configure, or by the compiler itself.  Still
    work to be done though :)

--- src/backend/commands/vacuum.c
+++ src/backend/commands/vacuum.c
@@ -7,7 +7,7 @@
  *
  *
  * IDENTIFICATION
- *	  $Header: /home/rubik/work/pgcvs/CVSROOT/pgsql/src/backend/commands/vacuum.c,v 1.54 1997-12-17 04:44:50 scrappy Exp $
+ *	  $Header: /home/rubik/work/pgcvs/CVSROOT/pgsql/src/backend/commands/vacuum.c,v 1.55 1997-12-19 02:05:33 scrappy Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -54,7 +54,7 @@
 #include <sys/resource.h>
 #endif
 
-/* #include <port-protos.h> */
+/* #include <port-protos.h> */ /* Why? */
 
 extern int BlowawayRelationBuffers(Relation rdesc, BlockNumber block);
 
