a36e9470cbb93cce493a3d3b01c5e2b8a5f3e597
momjian <momjian>
1998-09-07 05:35:48 +0000

    Alignment cleanup so no more massive switch statements for alignment, just two macros.

--- src/backend/access/index/indexam.c
+++ src/backend/access/index/indexam.c
@@ -7,7 +7,7 @@
  *
  *
  * IDENTIFICATION
- *	  $Header: /home/rubik/work/pgcvs/CVSROOT/pgsql/src/backend/access/index/indexam.c,v 1.26 1998-09-02 23:05:21 momjian Exp $
+ *	  $Header: /home/rubik/work/pgcvs/CVSROOT/pgsql/src/backend/access/index/indexam.c,v 1.27 1998-09-07 05:35:30 momjian Exp $
  *
  * INTERFACE ROUTINES
  *		index_open		- open an index relation by relationId
@@ -384,9 +384,7 @@ GetIndexValue(HeapTuple tuple,
 		*attNull = FALSE;
 	}
 	else
-	{
-		returnVal = heap_getattr(tuple, attrNums[attOff],
-								 hTupDesc, attNull);
-	}
+		returnVal = heap_getattr(tuple, attrNums[attOff], hTupDesc, attNull);
+
 	return returnVal;
 }
