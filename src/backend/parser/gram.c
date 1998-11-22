d1fc738d7158e26f9905ed376c2a0ddafa907214
vadim <vadim>
1998-11-22 10:48:45 +0000

    Fix using indices in OR. EXPLAIN all indices used.

--- src/backend/parser/gram.c
+++ src/backend/parser/gram.c
@@ -229,7 +229,7 @@
  *
  *
  * IDENTIFICATION
- *	  $Header: /home/rubik/work/pgcvs/CVSROOT/pgsql/src/backend/parser/Attic/gram.c,v 2.48 1998-10-30 04:54:01 scrappy Exp $
+ *	  $Header: /home/rubik/work/pgcvs/CVSROOT/pgsql/src/backend/parser/Attic/gram.c,v 2.49 1998-11-22 10:48:45 vadim Exp $
  *
  * HISTORY
  *	  AUTHOR			DATE			MAJOR EVENT
