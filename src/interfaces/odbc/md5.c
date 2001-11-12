1274824728c86bd7e62fe7e8549bc97cd2a54446
momjian <momjian>
2001-11-12 01:56:12 +0000

    Update md5 to match.

--- src/interfaces/odbc/md5.c
+++ src/interfaces/odbc/md5.c
@@ -10,7 +10,7 @@
  *
  *	Sverre H. Huseby <sverrehu@online.no>
  *
- * $Header: /home/rubik/work/pgcvs/CVSROOT/pgsql/src/interfaces/odbc/Attic/md5.c,v 1.1 2001-11-12 00:54:28 inoue Exp $
+ * $Header: /home/rubik/work/pgcvs/CVSROOT/pgsql/src/interfaces/odbc/Attic/md5.c,v 1.2 2001-11-12 01:56:12 momjian Exp $
  */
 
 
