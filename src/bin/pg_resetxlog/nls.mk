abcaf4603f0ee71c1e65ad29cb980d4ed3df769e
pgdude <pgdude>
2002-09-22 19:42:53 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.51.2'.

--- src/bin/pg_resetxlog/nls.mk
+++
@@ -1,5 +0,0 @@
-# $Header: /home/rubik/work/pgcvs/CVSROOT/pgsql/src/bin/pg_resetxlog/nls.mk,v 1.2 2002-09-14 13:46:24 petere Exp $
-CATALOG_NAME	:= pg_resetxlog
-AVAIL_LANGUAGES	:= ru
-GETTEXT_FILES	:= pg_resetxlog.c
-GETTEXT_TRIGGERS:= _
