de8adf8a32e7ea8ae29dd41b37812598d4a53600
momjian <momjian>
2003-03-20 15:39:53 +0000

    Remove compile warning.

--- src/bin/psql/common.c
+++ src/bin/psql/common.c
@@ -3,7 +3,7 @@
  *
  * Copyright 2000 by PostgreSQL Global Development Group
  *
- * $Header: /home/rubik/work/pgcvs/CVSROOT/pgsql/src/bin/psql/common.c,v 1.60 2003-03-20 06:43:35 momjian Exp $
+ * $Header: /home/rubik/work/pgcvs/CVSROOT/pgsql/src/bin/psql/common.c,v 1.61 2003-03-20 15:39:52 momjian Exp $
  */
 #include "postgres_fe.h"
 #include "common.h"
@@ -621,7 +621,7 @@ char parse_char(char **buf)
   long l;
 
   l = strtol(*buf, buf, 0);
-  (*buf)--;
+  --*buf;
   return (char)l;
 }
 
