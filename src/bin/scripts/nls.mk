b22e033e829d8f32032490d4ab60884876048c9c
petere <petere>
2003-07-23 08:49:30 +0000

    Update German translations.

--- src/bin/scripts/nls.mk
+++ src/bin/scripts/nls.mk
@@ -1,6 +1,6 @@
-# $Header: /home/rubik/work/pgcvs/CVSROOT/pgsql/src/bin/scripts/nls.mk,v 1.2 2003-06-18 12:19:11 petere Exp $
+# $Header: /home/rubik/work/pgcvs/CVSROOT/pgsql/src/bin/scripts/nls.mk,v 1.3 2003-07-23 08:49:30 petere Exp $
 CATALOG_NAME    := pgscripts
-AVAIL_LANGUAGES := 
+AVAIL_LANGUAGES := de
 GETTEXT_FILES   := createdb.c createlang.c createuser.c \
                    dropdb.c droplang.c dropuser.c \
                    clusterdb.c vacuumdb.c \
